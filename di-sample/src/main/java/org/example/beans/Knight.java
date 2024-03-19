59d26cb089bab834d0bb24e387db1e0fb5c04ab8
hlwan <hlwanmoeaung.501@gmail.com>
2024-03-19 15:23:18 +0630

    spring-learning

--- di-sample/src/main/java/org/example/beans/Knight.java
+++ di-sample/src/main/java/org/example/beans/Knight.java
@@ -1,5 +1,6 @@
 package org.example.beans;
 
+import org.example.annotation.QuestType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -7,14 +8,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Knight {
 
-    @Autowired /*@Qualifier("dragon")*/
-    private Quest dragon;
+    @Autowired @QuestType(QuestType.Type.YoYeYo)
+    private Quest quest;
 
   /*  public void setQuest(Quest quest) {
         this.quest = quest;
     }*/
 
     public String goQuest(){
-        return dragon.goQuest();
+        return quest.goQuest();
     }
 }
--- di-sample/src/main/java/org/example/beans/YoYeYoQuest.java
+++ di-sample/src/main/java/org/example/beans/YoYeYoQuest.java
@@ -1,13 +1,14 @@
 package org.example.beans;
 
+import org.example.annotation.QuestType;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component("yoyeyo")
+@Component @QuestType(QuestType.Type.YoYeYo)
 public class YoYeYoQuest implements Quest{
 
     @Override
     public String goQuest() {
-        return "Knighy sings 'YoYeYo' happily.";
+        return "Knight sings 'YoYeYo' happily.";
     }
 }
